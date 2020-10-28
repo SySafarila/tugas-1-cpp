567680e1ef56bbafff7f22c258839fecec5c1e79
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2020-10-28 20:20:54 +0700

    Update Menghitung luas lingkaran.cpp

--- Menghitung luas lingkaran.cpp
+++ Menghitung luas lingkaran.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
 	int jariJari;
+	int diameter = jariJari*2;
 	float pi = 3.14;
+	float first = 1/4;
 	cout << "Menghitung luas sebuah lingkaran. \n";
 	cout << "Masukan nilai jari-jari lingkaran : ";
 	cin >> jariJari;
-	cout << "Luas lingkaran berdasarkan nilai Jari-jari yang anda berikan yaitu " << jariJari << " adalah : " << 2*pi*jariJari;
+	cout << "Luas lingkaran berdasarkan nilai Jari-jari yang anda berikan yaitu " << jariJari << " adalah : " << pi*jariJari*jariJari;
 	
 	return 0;
 }
